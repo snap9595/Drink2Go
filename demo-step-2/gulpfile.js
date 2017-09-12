e4e3b7f56e7a5e353872619d3e6f72ecc7e1d031
katyamatya21 <katyamatya21@gmail.com>
2017-09-12 11:44:53 +0300

    Добавляет сборщик

---
+++ demo-step-2/gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    csscomb = require('gulp-csscomb'),
+    cssbeautify = require('gulp-cssbeautify');
+
+gulp.task('less', function () {
+  return gulp.src('./less/*.less')
+    .pipe(less())
+    .pipe(csscomb())
+    .pipe(cssbeautify({
+      indent: '  '
+    }))
+    .pipe(gulp.dest('./css'));
+});
